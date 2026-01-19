7f2fdabcfed88b3aca87b9512ca8a6ecaa547166
TokyHenintsoa00 <tokyramanalinarivo@gmail.com>
2026-01-19 21:57:20 +0300

    Formulaire de demande de rdv
    demande de rdv fomulaire front pas de back

--- frontend/src/app/pages/uikit/RendezVousComponent.ts
+++ frontend/src/app/pages/uikit/RendezVousComponent.ts
@@ -1,61 +1,80 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+
 import { ButtonModule } from "primeng/button";
+import { SelectModule  } from "primeng/select";
 import { FluidModule } from "primeng/fluid";
 import { InputTextModule } from "primeng/inputtext";
-import { SelectModule } from "primeng/select";
 import { TextareaModule } from "primeng/textarea";
-import { DropdownModule } from '.';
+import { FileUploadModule } from 'primeng/fileupload';
+
 @Component({
     selector: 'app-rdv-demo',
     standalone: true,
-    imports: [InputTextModule, FluidModule, ButtonModule, SelectModule, FormsModule, TextareaModule,
-             CommonModule,
+    imports: [
+        CommonModule,
         FormsModule,
         InputTextModule,
-        DropdownModule,
-        ButtonModule
+        ButtonModule,
+        SelectModule,
+        FluidModule,
+        TextareaModule,
+        FileUploadModule
     ],
-    template: `<p-fluid>
+    template: `
+<p-fluid>
     <div class="flex flex-col md:flex-row gap-8">
         <div class="md:w-1/2">
             <div class="card flex flex-col gap-6 shadow-2 border-round-xl">
 
                 <div class="font-semibold text-xl text-primary">
-                    Vertical Grid
+                    Demande de rendez-vous
                 </div>
 
                 <!-- Name & Email -->
                 <div class="flex flex-wrap gap-6">
                     <div class="flex flex-col grow basis-0 gap-2">
-                        <label for="name2" class="font-medium">Name</label>
-                        <input pInputText id="name2" type="text" placeholder="Enter your name" />
+                        <label for="name" class="font-medium">Name</label>
+                        <input pInputText id="name" type="text" placeholder="Enter your name" />
                     </div>
 
                     <div class="flex flex-col grow basis-0 gap-2">
-                        <label for="email2" class="font-medium">Email</label>
-                        <input pInputText id="email2" type="email" placeholder="Enter your email" />
+                        <label for="email" class="font-medium">Email</label>
+                        <input pInputText id="email" type="email" placeholder="Enter your email" />
                     </div>
                 </div>
 
-                <!-- Select -->
-                <div class="flex flex-col gap-2">
-                    <label for="role" class="font-medium">Role</label>
-                    <p-dropdown
-                        id="role"
-                        [options]="roles"
-                        optionLabel="label"
-                        placeholder="Select a role"
-                        class="w-full">
-                    </p-dropdown>
+                
+
+                <div>
+                    <label for="name" class="font-medium">Model de voiture</label>
+                    <input pInputText id="nom" type="text" placeholder="Constructeur-XXX-XXX" class="w-full md:w-120 mb-8"/>
+                
+                    <label for="name" class="font-medium">Definir le probleme de votre voiture</label>
+                    <textarea
+                        pInputTextarea
+                        id="description"
+                        rows="4"
+                        placeholder="Your family name"
+                        class="w-full md:w-120 mb-8">
+                    </textarea>
                 </div>
+
 
                 <!-- File -->
                 <div class="flex flex-col gap-2">
-                    <label for="file" class="font-medium">Upload file</label>
-                    <input type="file" id="file" class="w-full" />
+                    <label class="font-medium">Photo de la voiture</label>
+                    <p-fileUpload
+                        mode="basic"
+                        name="file"
+                        chooseLabel="Choose file"
+                        accept=".pdf,.jpg,.png"
+                        maxFileSize="5000000"
+                        class="w-full">
+                    </p-fileUpload>
                 </div>
+
 
                 <!-- Button -->
                 <div class="flex justify-end pt-2">
@@ -68,7 +87,12 @@ import { DropdownModule } from '.';
 </p-fluid>
 `
 })
+export class RendezVousComponent {
 
-export class RendezVousComponent{
-    roles = null;
+    roles = [
+        { label: 'Student', value: 'student' },
+        { label: 'Teacher', value: 'teacher' },
+        { label: 'Admin', value: 'admin' }
+    ];
+
 }
