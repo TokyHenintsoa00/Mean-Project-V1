d8ebb0b89489eea9079a639027525918e7db9591
TokyHenintsoa00 <tokyramanalinarivo@gmail.com>
2026-01-24 18:17:18 +0300

    front FORMULAIRE DE RDV

--- frontend/src/app/pages/service/rdv.service.ts
+++ frontend/src/app/pages/service/rdv.service.ts
@@ -12,6 +12,17 @@ export class RdvService{
     constructor(private http: HttpClient) {}
 
     rdvClient(rdv: any): Observable<any>{
+        const formData = new FormData();
+        formData.append('nom_client', rdv.nom_client);
+        formData.append('email_client', rdv.email_client);
+        formData.append('model_voiture', rdv.model_voiture);
+        formData.append('probleme_voiture', rdv.probleme_voiture);
+        formData.append('date_rendez_vous',rdv.date_rendez_vous);
+
+        rdv.photo_voiture.forEach((file:File) => {
+            formData.append('photo_voiture',rdv.photo_voiture)
+        });
+
         return this.http.post(`${this.apiUrl}/rdvClient` , rdv);
     }
 
